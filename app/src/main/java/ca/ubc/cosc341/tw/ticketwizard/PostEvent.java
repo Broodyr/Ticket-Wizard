e2cc300a1fa2159accb9195c66a08e84279d3002
Broodyr <broodyr@gmx.com>
2018-11-19 00:02:07 -0800

    Post Event interface prototype

--- app/src/main/java/ca/ubc/cosc341/tw/ticketwizard/PostEvent.java
+++ app/src/main/java/ca/ubc/cosc341/tw/ticketwizard/PostEvent.java
@@ -1,7 +1,11 @@
 package ca.ubc.cosc341.tw.ticketwizard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 
 public class PostEvent extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class PostEvent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_event);
+
+        TextView paragraph = findViewById(R.id.paragraph);
+        paragraph.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    public void back(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        finishAfterTransition();
+        startActivity(intent);
     }
 }
